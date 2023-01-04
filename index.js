ac90c06935a9912292c2cb6efcfb89f6810b67d9
Rebecca Wollard <rbwollard@gmail.com>
2023-01-04 11:43:44 -0700

    Update HTML to remove footer and update to form submission to render to DOM

--- index.js
+++ index.js
@@ -11,13 +11,11 @@ fetch('http://localhost:3000/details')
 }
 )
 
-
-
-
 const photoList = document.querySelector(`#photo-list`)
-const decadeStories = document.querySelector(`decade-dropdown-stories`)
+const decadeStories = document.querySelector(`#decade-dropdown-stories`)
 const decadeDropdown = document.querySelector(`#decade-dropdown`)
-
+const sightingsForm = document.querySelector(`#sightings-report`)
+const submitBtn = document.querySelector(`.btn-primary`)
 
 function renderSasquatchCard(sasquatchSighting) {
     const divElement = document.createElement("div");
@@ -55,27 +53,51 @@ function renderSasquatchCard(sasquatchSighting) {
     function addDislikes(btnElement) {
         numberDislikes++;
         btnElement.textContent = numberDislikes + ` ` + ` Dislikes 👎 `
-    
-    }
-    } 
+    }} 
 
 
-    decadeDropdown.addEventListener("change", (e) => {
+decadeDropdown.addEventListener("change", (e) => {
     selectDecade(e.target.value)
+})
+
+function renderSasquatchStories(decade) {
+    const liElement = document.createElement("li")
+    liElement.innerText = decades.description
+    decadeStories.append(liElement)
 }
-)
 
 function selectDecade(decade) {
     console.log(`decade:`, decade)
-}
+  if (decade > `1950` && decade > `1960`) {
+    renderSasquatchStories(decade)
+  } else if(decade >'1960' && decade < '1970') {
+    return 'That will be twenty bucks.'
+}}
 
 
-function renderSasquatchStories(sasData) {
-    const divElement = document.createElement("div")
-    divElement.classList.add("sasquatch-story");
-    divElement.innerText = sasData.description
-    decadeStories.append(divElement)
-}
+
+function SasquatchStory(year) {
+    const dates = [Object.keys(decades)]
+    const date = (year) => dates().find(date => date.allDates === year)
+    return dates(year).description
+} 
+
+
+
+// function goodPractices() {
+//     let game = gameObject();
+//     for (let gameKey in game) {
+
+//       let teamObj = game[gameKey];
+//       for (let teamKey in teamObj) {
+
+//         let data = teamObj.player;
+//         for (let key in data) 
+//       }
+         
+// function findMatchingDecade(date) {
+//     const
+// }
 
 
 // function scuberGreetingForFeet(distance){
@@ -90,28 +112,27 @@ function renderSasquatchStories(sasData) {
 //   }
 // }
 
+sightingsForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = e.target.name.value
+    const date = e.target.date.value
+    const location = e.target.location.value
+    const image = e.target.image.value
+    const description = e.target["sighting-story"].value
+    const likes = "0"
+    const dislikes = "0"
 
-// let rangeA = {
-//     lower: 1950,
-//     upper: 1960
-// }
+    const newStory = {
+        name,
+        date,
+        location,
+        image,
+        description,
+        likes,
+        dislikes
+    }
+    renderSasquatchCard(newStory)
+    sightingsForm.reset()
+})
 
-// let rangeB = {
-//     lower: 1960,
-//     upper: 1970
-// }
 
-// let rangeC = {
-//     lower: 1970,
-//     upper: 1980
-// }
-
-// let rangeD = {
-//     lower: 1980,
-//     upper: 1990
-// }
-
-// let rangeE = {
-//     lower: 1990,
-//     upper: 2000
-// }
